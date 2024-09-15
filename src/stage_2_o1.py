20fa02acba2183ee1009d0bc79df1ef1b2c32713
josip <josip.zivic93@gmail.com>
2024-09-15 17:49:35 +0200

    c22
    Signed-off-by: josip <josip.zivic93@gmail.com>

--- src/stage_2_o1.py
+++ src/stage_2_o1.py
@@ -1,4 +1,5 @@
 import sympy
+from numpy.ma.testutils import fail_if_equal
 
 from stage_0 import *
 from stage_1 import *
@@ -17,7 +18,7 @@ class CalculateBeam(Prepare_Loads):
         self.matrix_sum = []  # Right-hand side (sums)
 
         # Calculate the equilibrium equations (force and moment) and solve
-        self.f_values, self.f_locations = self.forces_equation()  # This adds the sum of forces first!
+        # self.f_values, self.f_locations = self.forces_equation()  # This adds the sum of forces first!
         self.equilibrium_equations()  # Then we add the moment equations
 
         # Solve the system of equations
@@ -37,85 +38,55 @@ class CalculateBeam(Prepare_Loads):
         return variables
 
 
-    def forces_equation(self):
-        # Sum of applied forces (F and equivalent forces from distributed loads)
-        sum_f = sum([v["value"] for v in self.sorted_loads.values() if v["type"] == "F"])
-
-
-        f_values = []  # Store forces
-        f_locations = []  # Store force positions
-
-        for key, load in self.sorted_loads.items():
-            if load["type"] == "F":  # Point force
-                f_values.append(-load["value"])  # Negative for equilibrium
-                f_locations.append(load["position"])
-            elif load["type"] == "q":  # Distributed load (with equivalent force)
-                f_values.append(-load["F_eqv"])  # Negative for equilibrium
-                f_locations.append(load["x_F_eqv"])
-
-        # Add the vertical equilibrium equation (∑F = 0) to the first row
-        f_eq = [0] * len(self.variables)
-        for i, (var_name, var_info) in enumerate(self.variables.items()):
-            if var_info["type"] == "R":  # Reaction force
-                f_eq[i] = 1  # Coefficient for reaction forces
-
-        # Append the force equilibrium equation FIRST to the matrix
-        self.matrix_eq.insert(0, f_eq)
-        self.matrix_sum.insert(0, [sum_f])
-
-        return f_values, f_locations
-
-
-
 
 
 
     def equilibrium_equations(self):
 
         sum_f = sum([v["value"] for v in self.sorted_loads.values() if v["type"] == "F"])
-        sum_M = sum(v["value"] for v in self.sorted_loads.values() if v["type"] == "M")
-
-        # Add moment equilibrium equations for each support
-        m_locations = [var["location"] for var in self.variables.values() if var["type"] == "F"]
+        matrix_sum = [[sum_f]]
 
         # PRVO IDE JEDNADŽBA SILA
-        # Loop through the support points to create moment equations
-
-        a = []
-        for key, value in self.variables.items():
-            if value["type"] == "F":
-                a.append()
+        f_eq = [1 for v in self.variables.values() if v["type"] == "F"]
+        f_eq.extend([0 for v in self.variables.values() if v["type"] == "M"])
+        matrix_eq = [f_eq]
 
 
+        # MOMNENTNA JEDNADŽBA
+        sum_M_extern = sum(v["value"] for v in self.sorted_loads.values() if v["type"] == "M")
+        m_locations = [var["location"] for var in self.variables.values() if var["type"] == "F"]
 
-        # for x_pos in m_locations:
-        #     m_eq = [0] * len(self.variables)  # Moment equation
-        #     total_moment_sum = 0  # Total moment sum
-        #
-        #     # Reaction force moments
-        #     for i, (var_name, var_info) in enumerate(self.variables.items()):
-        #         if var_info["type"] == "R":  # Reaction force
-        #             dist = var_info["location"] - x_pos
-        #             m_eq[i] = dist  # Moment arm
-        #         elif var_info["type"] == "M":  # Reaction moment
-        #             m_eq[i] = 1 if var_info["location"] == x_pos else 0
-        #
-        #
-        #
-        #
-        #
-        #     # Moments due to applied forces and distributed loads
-        #     for f_loc, f_value in zip(self.f_locations, self.f_values):
-        #         dist = f_loc - x_pos
-        #         total_moment_sum += dist * f_value
-        #
-        #     # Add external moments to the sum
-        #     total_moment_sum += ext_M_sum
-        #
-        #     # Append the moment equation and its sum to the matrix
-        #     self.matrix_eq.append(m_eq)
-        #     self.matrix_sum.append([total_moment_sum])
-        #
+        for x_pos in m_locations:
+            sum_M_position = sum_M_extern
+
+            m_eq = [None] * len(self.variables)  # Moment equation
+
+            for n, (var_name, var_info) in enumerate(self.variables.items()):
+                if var_info["type"] == "F":  # Reaction force
+                    dist = var_info["location"] - x_pos
+                    m_eq[n] = dist  # Moment arm
+
+                elif var_info["type"] == "q":  # Reaction force
+                    dist = var_info["location"] - x_pos
+                    m_eq[n] = dist  # Moment arm
+
+
+                elif var_info["type"] == "M":  # Reaction force
+                    m_eq[n] = 1
+
+            matrix_eq.append(m_eq)
+
+            # ovdje treba ić suma u točki x_pos
+            for k, v in filter(lambda item: item[1]["type"] == "F", self.sorted_loads.items()):
+                sum_M_position += v["value"] * ( v["position"] - x_pos)
+
+            matrix_sum.append([sum_M_position])
+
+
+        X, residuals, rank, s = np.linalg.lstsq(matrix_eq, matrix_sum, rcond=None)
+        print(X)
+
+
         # print(f"Matrix Eq: {self.matrix_eq}")
         # print(f"Matrix Sum: {self.matrix_sum}")
 
